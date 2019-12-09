417b7723f091019468f5bbdaec6c54ae6e6fb142
zhouyunpeng <zhouyunpeng@huya.com>
2019-12-09 18:51:44 +0800

    修复部分弹窗无法解决的问题

--- WebDriverAgentLib/Commands/FBElementCommands.m
+++ WebDriverAgentLib/Commands/FBElementCommands.m
@@ -104,25 +104,37 @@
 {
    XCUIApplication* application = FBApplication.fb_activeApplication;
    CGPoint tapPoint = CGPointMake((CGFloat)[request.arguments[@"x"] doubleValue], (CGFloat)[request.arguments[@"y"] doubleValue]);
-  NSTimeInterval duration = [request.arguments[@"duration"] doubleValue];
-  XCUIElement *element = application.windows.fb_firstMatch;
-
-   CGSize frameSize = application.frame.size;
-   UIInterfaceOrientation orientation = application.interfaceOrientation;
-  
-  if(isSDKVersionLessThan(@"11.0")){
+   NSTimeInterval duration = [request.arguments[@"duration"] doubleValue];
+   XCUIElement *element = application.windows.fb_firstMatch;
+   
+   if(isSDKVersionLessThan(@"11.0")){
+     CGSize frameSize = application.frame.size;
+     UIInterfaceOrientation orientation = application.interfaceOrientation;
      tapPoint = FBInvertPointForApplication(tapPoint, frameSize, orientation);
-  }
-   XCUICoordinate *appCoordinate = [[XCUICoordinate alloc] initWithElement:element normalizedOffset:CGVectorMake(0, 0)];
-   XCUICoordinate *tapCoordinate  = [[XCUICoordinate alloc] initWithCoordinate:appCoordinate pointsOffset:CGVectorMake(tapPoint.x, tapPoint.y)];
-  if(duration>0.5){
-    [tapCoordinate pressForDuration:duration];
-  }
-  else{
-    [tapCoordinate tap];
-  }
-
-  return FBResponseWithOK();
+   }
+   if(duration>0.5 || SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"13.0")){
+     XCUICoordinate *appCoordinate = [[XCUICoordinate alloc] initWithElement:element normalizedOffset:CGVectorMake(0, 0)];
+     XCUICoordinate *tapCoordinate  = [[XCUICoordinate alloc] initWithCoordinate:appCoordinate pointsOffset:CGVectorMake(tapPoint.x, tapPoint.y)];
+     if(duration>0.5){
+        [tapCoordinate pressForDuration:duration];
+     }
+     else{
+        [tapCoordinate tap];
+     }
+   } else {
+    NSArray<NSDictionary<NSString *, id> *> *tapGesture =
+    @[
+        @{@"action": @"tap",
+          @"options": @{
+            @"element": element,
+            @"x": @(tapPoint.x),
+            @"y": @(tapPoint.y)
+            }
+        }
+    ];
+    [application fb_performAppiumTouchActions:tapGesture elementCache:nil error:nil];
+   }
+   return FBResponseWithOK();
 }
 
 + (id<FBResponsePayload>)handleDragCoordinate_stf:(FBRouteRequest *)request
